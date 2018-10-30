52519585dcf16c805b1b81a2b1910b58868ac34f
alexHooper <alexmohack@gmail.com>
2018-10-30 16:15:20 +0530

    separate accounts urls

--- backend/urls.py
+++ backend/urls.py
@@ -1,17 +1,14 @@
 from django.contrib import admin
-from django.urls import path
-from django.contrib.auth.views import LoginView
+from django.urls import path, include
+from django.contrib.auth.views import LoginView, LogoutView
 
 from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
-
-from accounts.views import signup_view, dashboard_view, home_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', LoginView.as_view(), name='login'),
-    path('', home_view, name='home'),
-    path('dashboard/', dashboard_view, name='home'),
-    path('register/', signup_view, name='home'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('', include('accounts.urls', namespace='accounts')),
 ]
 
 urlpatterns += [

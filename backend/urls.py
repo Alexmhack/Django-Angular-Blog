e6df0c193c150451004b74d0ab1b8c0fc6daea8f
alexHooper <alexmohack@gmail.com>
2018-10-30 15:30:58 +0530

    add url for login view

--- backend/urls.py
+++ backend/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth.views import LoginView
 
 from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', LoginView.as_view(), name='login'),
 ]
 
 urlpatterns += [

0d137c95f0248249f5bc99e136180377a52e5af4
alexHooper <alexmohack@gmail.com>
2018-10-30 16:04:31 +0530

    add views for signup

--- backend/urls.py
+++ backend/urls.py
@@ -4,9 +4,14 @@ from django.contrib.auth.views import LoginView
 
 from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
 
+from accounts.views import signup_view, dashboard_view, home_view
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', LoginView.as_view(), name='login'),
+    path('', home_view, name='home'),
+    path('dashboard/', dashboard_view, name='home'),
+    path('register/', signup_view, name='home'),
 ]
 
 urlpatterns += [

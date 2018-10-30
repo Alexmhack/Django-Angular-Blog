bb3d6062a4d8f9bdaa25c12293a2225464bc88a3
alexHooper <alexmohack@gmail.com>
2018-10-30 14:24:07 +0530

    create custom user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
-class User()
+class User(AbstractUser):
+	email = models.EmailField(_('email address'), max_length=150)
